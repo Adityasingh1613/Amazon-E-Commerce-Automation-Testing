02d9e5cd8dfaeab07031841d39d871b5198420f0
Adityasingh1613 <2416adityasingh@gmail.com>
2025-08-18 12:04:11 +0530

    util add

---
+++ Amazon_Automation_project/src/test/java/product/utils/waitUtil.java
@@ -0,0 +1,16 @@
+package product.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class waitUtil {
+
+    public static void waitforElement(WebDriver driver, WebElement element, int time) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(time));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+}
