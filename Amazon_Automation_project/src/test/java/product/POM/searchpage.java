afa0a61da04a70e3bab8e12a9b35ad9e42593e50
Adityasingh1613 <2416adityasingh@gmail.com>
2025-08-20 19:25:07 +0530

    test case 3 added

--- Amazon_Automation_project/src/test/java/product/POM/searchpage.java
+++ Amazon_Automation_project/src/test/java/product/POM/searchpage.java
@@ -23,6 +23,9 @@ public class searchpage extends basepage{
     @FindBy(xpath = "//h2[@aria-label='iPhone 16 Pro Max 256 GB: 5G Mobile Phone with Camera Control, 4K 120 fps Dolby Vision and a Huge Leap in Battery Life. Works with AirPods; Natural Titanium']")
     WebElement Iphone;
 
+    @FindBy(xpath = "//div[@class='puis-card-container s-card-container s-overflow-hidden aok-relative puis-expand-height puis-include-content-margin puis puis-v2dedio7i062j12oa38qtc8evog s-latency-cf-section puis-card-border']//h2[@aria-label='Sponsored Ad - Perfora PRO Oscillating Electric Toothbrush | 2 Year Warranty | Electric Toothbrush Rechargeable, Electric Brush For Men & Women, 8800 RPM, 1 Mode, 2 min Auto Timer (Limitless Black)']//span[contains(text(),'Perfora PRO Oscillating Electric Toothbrush | 2 Ye')]")
+    WebElement brush;
+
     @FindBy(xpath = "//div[@class='a-section a-spacing-none a-padding-none']//input[@id='add-to-cart-button']")
     WebElement addtocart;
 
@@ -57,6 +60,14 @@ public class searchpage extends basepage{
 
     public void AmazonPage(){
         Amazon.click();
+    }
+
+    public WebElement setBrush(){
+        return brush;
+    }
+
+    public void getBrush(){
+        brush.click();
     }
 
 }
--- Amazon_Automation_project/src/test/java/product/Test/TC003_ElectricToothbrush.java
+++ Amazon_Automation_project/src/test/java/product/Test/TC003_ElectricToothbrush.java
@@ -1,8 +1,56 @@
 package product.Test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import product.POM.searchpage;
 import product.Testbase.Base;
+import product.utils.screenshotUtil;
+
+import java.io.IOException;
 
 public class TC003_ElectricToothbrush extends Base {
+    private static final Logger logger = LogManager.getLogger(TC003_ElectricToothbrush.class);
+
+    @Test
+    public void burshsearch() throws InterruptedException{
+        logger.info("** Strating Test TC003_ElectricToothbrush **");
+
+        try{
+            searchpage sp = new searchpage(driver);
+
+            sp.setSearchbox("Electric ToothBrush");
+            logger.info("Search for electric toothbrush");
+
+            sp.setSearchbtn();
+
+            Actions act = new Actions(driver);
+            act.moveToElement(sp.setBrush()).perform();
+            logger.info("Scrolled to the Product using Action class");
+
+            sp.getBrush();
+            logger.info("Clicked on the product");
+
+            sp.setAddtocart();
+            logger.info("Product added to the Cart");
+
+            Thread.sleep(5000);
 
 
+        }catch (Exception e){
+            logger.error("Test Failed due to exception: ", e);
+
+            try {
+                screenshotUtil.takescreenshot(driver, "TC003_ElectricToothbrush_Failure");
+                logger.info("Screenshot captured for failure");
+            } catch (IOException io) {
+                logger.error("Screenshot capture failed: ", io);
+            }
+            Assert.fail("Test failed due to exception: " + e.getMessage());
+        }
+
+        logger.info("Finished TC003_ElectricToothbrush ");
+    }
 }
